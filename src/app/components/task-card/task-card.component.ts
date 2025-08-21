88197dded71e3e29f7b65b2be4ad5524c3353a1e
graywheels <graywheeler4@gmail.com>
2025-08-21 15:57:08 -0600

    more phase7 taskcard

--- src/app/components/task-card/task-card.component.ts
+++ src/app/components/task-card/task-card.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from "@angular/material/icon";
+import { boolean } from 'zod/v4';
+import { Task } from '../../../../prisma/client';
+import { Component, effect, inject, input, linkedSignal, output, signal } from '@angular/core';
+import { trpcResource } from '@fhss-web-team/frontend-utils';
+import { MatFormField, MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+// import { TRPC_CLIENT } from '../../../utils/trpc.client';
+// import { TaskStatus } from '../../../enums/task-status';
+// import { StatusMenuComponent } from "../status-menu/status-menu.component";
 
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatInputModule, FormsModule],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss'
 })
-export class TaskCardComponent {
 
+export class TaskCardComponent {
+  editMode = signal<boolean>(false);
+  initialTaskValue = input.required<Task>(); //declares an input signal of type Task. 
+  // The required marker means the parent component will error if it does not provide a Task.
+  newTitle = linkedSignal(() => this.initialTaskValue().title); // a linkedSignal needs a computation function to be created. This function should return a value that the signal can be set to.
+  newDescription = linkedSignal(() => this.initialTaskValue().description);
 }
--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -5,10 +5,11 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { TaskCardComponent } from '../../components/task-card/task-card.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [MatProgressSpinnerModule, MatPaginator, MatIconModule, MatButtonModule],
+  imports: [MatProgressSpinnerModule, MatPaginator, MatIconModule, MatButtonModule, TaskCardComponent],
   templateUrl: './tasks.page.html',
   styleUrl: './tasks.page.scss'
 })
@@ -28,6 +29,7 @@ export class TasksPage {
     }),
     { autoRefresh: true }
   );
+getTasks: any;
 
   handlePageEvent(e: PageEvent) {
     this.pageOffset.set(e.pageIndex * e.pageSize);
